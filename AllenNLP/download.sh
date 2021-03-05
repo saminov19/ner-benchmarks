2920b09ead170c4e23f1e007a6fd21c5f9516015
Sergei Mukhin <muhin0704@gmail.com>
2021-03-05 08:52:09 +0000

    Обновить datasets/SEC-filings/NOTE.txt, datasets/SEC-filings/README.txt, datasets/SEC-filings/CONLL-format/data/test/FIN3.txt, datasets/SEC-filings/CONLL-format/data/train/FIN5.txt, datasets/SEC-filings/CONLL-format/docs/entity-list.txt, datasets/SEC-filings/CONLL-format/docs/corpusconfig.cfg, datasets/conll/conll2003/test.txt, datasets/conll/conll2003/valid.txt, datasets/conll/conll2003/train.txt, datasets/ncbi/NCBI_corpus_development.txt, datasets/ncbi/NCBI_corpus_testing.txt, datasets/ncbi/NCBI_corpus_training.txt, datasets/ncbi/NCBItrainset_corpus.txt, deploy/docker/dockerfiles/flair/Dockerfile, deploy/docker/dockerfiles/flair/pyproject.toml, deploy/docker/dockerfiles/base/Dockerfile, deploy/docker/dockerfiles/base/pyproject.toml, deploy/docker/dockerfiles/spacy/Dockerfile, deploy/docker/dockerfiles/spacy/pyproject.toml файлов Удалено AllenNLP/.gitkeep, AllenNLP/download.sh, Stanford NER/.gitkeep, Flair NER/.gitkeep, Stanford_ner_complete.ipynb, f1_scores.md, flair_ner_complete.ipynb файлов

--- AllenNLP/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -o train.txt \
-  https://raw.githubusercontent.com/synalp/NER/master/corpus/CoNLL-2003/eng.train
-curl -o validation.txt \
-  https://raw.githubusercontent.com/synalp/NER/master/corpus/CoNLL-2003/eng.testa
-curl -o test.txt \
-  https://raw.githubusercontent.com/synalp/NER/master/corpus/CoNLL-2003/eng.testb
--- f1_scores.md
+++
@@ -1,9 +0,0 @@
-Data \ Entity type | Location | Person | Organization | Misc
---- | --- |--- | --- | ---
-**Stanford NER** | | | |
-CONLL 2003 pre-trained | 90 | 95 | 86 | 80
-SEC-Filings pre-trained | 40 | 29.7 | 26.1 | 4.3
-SEC-Filings custom-trained | 39 | 90 | 25.2 | 0
- **HunFlair NER**|  **Disease**| | |
-NCBI-Disease |87 | | |
-
