2143f65bab89e0c9e6edbaabb0a72dc05d6da5a2
Sultan Aminov <sultan_aminov@epam.com>
2021-04-08 17:25:07 +0600

    Commit

---
+++ src/notebooks/Allen NLP/allen/data/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -o train.txt \
+  https://raw.githubusercontent.com/saminov19/nlp_ner_tasks/main/train.txt
+curl -o validation.txt \
+  https://raw.githubusercontent.com/saminov19/nlp_ner_tasks/main/valid.txt
+curl -o test.txt \
+  https://raw.githubusercontent.com/saminov19/nlp_ner_tasks/main/test.txt
